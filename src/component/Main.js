dfc4f26ac8025a8c23dd08bd011b035cb400d989
ayh9601 <ays9499@naver.com>
2021-12-26 06:50:08 +0900

    stage9 main3 News

--- src/component/Main.js
+++ src/component/Main.js
@@ -24,6 +24,54 @@ function Main(){
                     </div>
                 </div>
 
+                <div className="latestStories">
+                    <h3>Latest Stories</h3>
+
+                    <article>
+                        <h4>Science</h4>
+                        <ul>
+                            <li className="LSTitle">
+                                How Does a Virus Inhibitor Work
+                            </li>
+                            <li className="LSArticle">
+                                The FDA has authorized Umbrella's COVID-20 oral treatment for emergency use.</li>
+                        </ul>
+                        <div className="LSPic">
+                            <div className="articlePic1">
+                            </div>
+                        </div>
+                    </article>
+
+                    <article>
+                        <h4>Our<br></br>Products</h4>
+                        <ul>
+                            <li className="LSTitle">Finally, A Cure For Progeria Has Been Completed.</li>
+                            <li className="LSArticle">
+                                After the efforts of many scientists, we have created a treatment using a new virus.</li>
+                        </ul>
+                        <div className="LSPic">
+                            <div className="articlePic2">
+                            </div>
+                        </div>
+                    </article>
+
+                    <article>
+                        <h4>Our<br></br>Purpose</h4>
+                        <ul>
+                            <li className="LSTitle">Umbrella's Institue Vision</li>
+                            <li className="LSArticle">We will protect humanity with an umbrella.</li>
+                        </ul>
+                        <div className="LSPic">
+                            <div className="articlePic3">
+                            </div>
+                        </div>
+                    </article>
+
+                    <article>
+                        <button>See More News</button>
+                    </article>
+                </div>
+
                 <div className="three">
                     <h3>eBook</h3>
                     <div className="threeLeft"></div>
