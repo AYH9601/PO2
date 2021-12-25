dca7f82b9a26251aca902e76bd532d4c8cc36332
ayh9601 <ays9499@naver.com>
2021-12-26 06:29:17 +0900

    stage7 Main2 & img Update

--- src/component/Main.js
+++ src/component/Main.js
@@ -10,20 +10,42 @@ function Main(){
                     Explore Our Science
                 </button>
             </div>
+
             <div className="inner">
                 <div className="two">
                     <h3>Featured Article</h3>
                     <div className="twoLeft"></div>
                     <div className="twoRight">
                         <h4>The Art of Creating<br></br>New Medicines</h4>
-                        <p>Scientists are often lauded for their objectivity, persistence, and analytic thinking. But great scientists have something else in common: they think like artists. And for those who create life-changing medicines, their process is as much an art form as a science. </p>
+                        <p>
+                            Scientists are often lauded for their objectivity, persistence, and analytic thinking. But great scientists have something else in common: they think like artists. And for those who create life-changing medicines, their process is as much an art form as a science. 
+                        </p>
                         <button>Go Behind the Science</button>
                     </div>
                 </div>
 
+                <div className="three">
+                    <h3>eBook</h3>
+                    <div className="threeLeft"></div>
+                    <div className="threeRight">
+                        <h4>Are You Making the Most of Patient Centricity?</h4>
+                        <p>
+                            Healthcare opportunities are only useful to you if you know they exist and how they work. This downloadable eBook is designed to help you make informed healthcare decisions.
+                        </p>
+                        <button>Learn How to Be an Empowerd, Engaged Patient</button>
+                    </div>
+                </div>
 
-
-
+                <div className="four">
+                    <h3>The Umbrella Store</h3>
+                    <div className="fourLeft"></div>
+                    <div className="fourRight">
+                        <h4>
+                            Show Your Pfizer Pride! Shop and share the collection.Available in the U.S. only.<br></br> <br></br>All profits from The Umbrella Store will be donated to charity.
+                        </h4>
+                        <button>Shop The Umbrella Collection</button>
+                    </div>
+                </div>
             </div>
         </section>
     )
