d88cac0b3e95d1c2a246b6c7615d7a2886d2559c
ayh9601 <ays9499@naver.com>
2021-12-26 04:59:24 +0900

    stage5 gallery4

--- src/sub/Gallery.js
+++ src/sub/Gallery.js
@@ -96,16 +96,53 @@ function Gallery(){
                     </article>
                 </div>
 
-                <div className="LatestStories">
-                    <ariticle>
+                <div className="latestStories">
+                    <h3>Latest Stories</h3>
+
+                    <article>
                         <h4>Science</h4>
-                    </ariticle>
-                    <ariticle>
+                        <ul>
+                            <li className="LSTitle">
+                                How Does a Virus Inhibitor Work
+                            </li>
+                            <li className="LSArticle">
+                                The FDA has authorized Umbrella's COVID-20 oral treatment for emergency use.</li>
+                        </ul>
+                        <div className="articlepic">
+                            <div className="pic">
+                                <img src="" />
+                            </div>
+                        </div>
+                    </article>
+                    <article>
                         <h4>Our<br></br>Products</h4>
-                    </ariticle>
-                    <ariticle>
+                        <ul>
+                            <li className="LSTitle">Finally, A Cure For Progeria Has Been Completed.</li>
+                            <li className="LSArticle">
+                                After the efforts of many scientists, we have created a treatment using a new virus.</li>
+                        </ul>
+                        <div className="articlepic">
+                            <div className="pic">
+                                <img src="" />
+                            </div>
+                        </div>
+                    </article>
+                    <article>
                         <h4>Our<br></br>Purpose</h4>
-                    </ariticle>
+                        <ul>
+                            <li className="LSTitle">Umbrella's Institue Vision</li>
+                            <li className="LSArticle">We will protect humanity with an umbrella.</li>
+                        </ul>
+                        <div className="articlepic">
+                            <div className="pic">
+                                <img src="" />
+                            </div>
+                        </div>
+                    </article>
+
+                    <article>
+                        <button>News Hub</button>
+                    </article>
                 </div>
             </div>
         </section>
