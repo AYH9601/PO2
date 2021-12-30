78c40df59183faba3d7c2b4b60af48cb220db712
ayh9601 <ays9499@naver.com>
2021-12-30 21:44:30 +0900

    stage20 Board

--- src/sub/Board.js
+++ src/sub/Board.js
@@ -5,7 +5,7 @@ function Board(){
     let [posts,setPosts] = useState([]);
     let len = posts.length;
     const path = process.env.PUBLIC_URL;
-    const url = `${path}/dbs/board.json`
+    const url = `${path}/dbs/board.json`;
 
     useEffect(()=>{
         axios
@@ -19,20 +19,45 @@ function Board(){
     return (
         <section className="content board">
             <div className="inner">
-                <h1>Board</h1>
-                {
-                    posts.slice(0).reverse().map((data, index)=>{
-                        return (
-                            <article key={index} className="board">
-                                <h1>{len-index}</h1>
-                                <h2>{data.title}</h2>
-                                <strong>{data.writer}</strong>
-                                <span>{data.date}</span>
-                                <em>{data.like}</em>
-                            </article>
-                        )
-                    })
-                }
+                <p>Information Board</p>
+                
+                <div className="boardInner">
+                    <div className="boardLeft">
+                        <h1>
+                        Browse our community and find information about the products that are best for you.
+                        </h1>
+
+                        <article id="BoardSearchBox">
+                            <input type="text" id="search" placeholder="Looking for a Symptoms?"/>
+                            <button className="btnSearch">
+                                <div className="arrowCircle">
+                                    <i className="fas fa-arrow-right"></i>
+                                </div>
+                            </button>
+                        </article>
+                    </div>
+
+                    <div className="boardRight">
+                        {
+                            posts.slice(0).reverse().map((data, index)=>{
+                                return (
+                                    <article key={index} className="boardDown">
+                                        <div className="boardDownPost">
+                                            <div className="boardDownNum">
+                                                <h1>{len-index}</h1>
+                                            </div>
+
+                                            <div className="boardDownTitle">
+                                                <h2>{data.title}</h2>
+                                                <h3>{data.date}</h3>
+                                            </div>
+                                        </div>
+                                    </article>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
             </div>
         </section>
     )
