21bde4715cdd0e49c713eae170e9be8153a2b775
ayh9601 <ays9499@naver.com>
2022-01-02 19:27:07 +0900

    stage30 board Update1

--- src/sub/Board.js
+++ src/sub/Board.js
@@ -34,6 +34,20 @@ function Board(){
                                 </div>
                             </button>
                         </article>
+
+                        <ul className="boardTag">
+                            <li>Vaccine</li>
+                            <li>Science</li>
+                            <li>Medicine</li>
+                            <li>Covid-20</li>
+                            <li>T-virus</li>
+                            <li>Hospital</li>
+                            <li>BIOHAZARD</li>
+                            <li>Disease</li>
+                            <li>CDC</li>
+                            <li>Pandemic</li>
+                            <li>Transport</li>
+                        </ul>
                     </div>
 
                     <div className="boardRight">
@@ -58,6 +72,54 @@ function Board(){
                                 )
                             })
                         }
+                    </div>
+                </div>
+
+                <div className="boardLower">
+                    <p>
+                        Community Board
+                    </p>
+                    <table summary="community">
+                        <thead>
+                            <tr>
+                                <th>Number</th>
+                                <th>Title</th>
+                                <th>ID</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                posts.slice(0).reverse(0).map((data, index)=>{
+                                    return (
+                                        <tr key={index} className="communityInner">
+                                            <td>{len-index}</td>
+                                            <td>{data.title}</td>
+                                            <td>{data.writer}</td>
+                                            <td>{data.date}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+
+                    <div className="pagination">
+                        <p>
+                            <div className="leftCircle">
+                                <i className="fas fa-arrow-left"></i>
+                            </div>
+                        </p>
+                        <p>1</p>
+                        <p>2</p>
+                        <p>3</p>
+                        <p>4</p>
+                        <p>5</p>
+                        <p>
+                            <div className="rightCircle">
+                                <i className="fas fa-arrow-right"></i>
+                            </div>
+                        </p>
                     </div>
                 </div>
             </div>
