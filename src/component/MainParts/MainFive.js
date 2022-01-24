7ee88938ed59f176da050b04a0982745bc061627
ayh9601 <ays9499@naver.com>
2022-01-24 19:48:51 +0900

    stage64 Swiper1

--- src/component/MainParts/MainFive.js
+++ src/component/MainParts/MainFive.js
@@ -1,3 +1,10 @@
+import { Navigation, Pagination, EffectCoverflow } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 function MainFive(){
     return(
         <div className="five myScroll">
@@ -7,6 +14,52 @@ function MainFive(){
             </span>
             <button>More On COVID-20</button>
 
+            <Swiper className='swiperSlide'
+                // install Swiper modules
+                modules={[Navigation, Pagination]}
+                spaceBetween={25}
+                slidesPerView={"auto"}
+                loop
+                // grabCursor
+                navigation
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                >
+                    <SwiperSlide className='swiperSlideBox'>
+                        <div className="fivePic1"></div>
+                        <h1>Our Vaccine Efforts</h1>
+                        <p>
+                            Follow the latest progress in the development, manufacturing, and distribution of our vaccine to help protect against the novel coronavirus.
+                        </p>
+                        <strong>Learn About Our Vaccine Efforts</strong>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiperSlideBox'>
+                        <div className="fivePic2"></div>
+                        <h1>Our Antiviral Efforts</h1>
+                        <p>
+                            If successful, antivirals can help slow or stop a virus from replicating and thereby reduce the symptoms associated with COVID-20 and the risk of significant health complications.
+                        </p>
+                        <strong>Learn About Promising Antivirals</strong>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiperSlideBox'>
+                        <div className="fivePic3"></div>
+                        <h1>Collaborating to Address COVID-20</h1>
+                        <p>
+                            We’re collaborating across the healthcare innovation ecosystem to help advance research and address the COVID-20 global health crisis.
+                        </p>
+                        <strong>How We Collaborate to Address the COVID-20 Crisis</strong>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiperSlideBox'>
+                        <div className="fivePic4"></div>
+                        <h1>Supporting Those Fighting on the Front Lines</h1>
+                        <p>
+                            The Pfizer Foundation is supporting our partners who are working tirelessly to address the evolving health needs related to COVID-20 in the U.S. and around the world.
+                        </p>
+                        <strong>Learn About The Umbrella Foundation</strong>
+                    </SwiperSlide>
+            </Swiper>
+
+{/* 
             <div className="fiveLower">
                 <ul>
                     <li>
@@ -37,7 +90,7 @@ function MainFive(){
                     </li>
                 </ul>
             </div>
-
+ */}
             <div className="pagination">
                 <div className="leftCircle">
                     <i className="fas fa-arrow-left"></i>
